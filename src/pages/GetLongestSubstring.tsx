a31070180ec3d3112729f67e1474cb5d58572c32
juliasuzanne <julia.s.grimes@gmail.com>
2024-08-06 20:28:35 -0400

    Fix edge case

--- src/pages/GetLongestSubstring.tsx
+++ src/pages/GetLongestSubstring.tsx
@@ -6,20 +6,23 @@ export default function GetLongestSubstring(){
 
   const getLongestSubstring = (testString: string) => {
     let prevString = [];
-    let longestString = [testString[0]];
+    let longestString = [];
     let prevRecord = 0;
     let currentRecord = 0;
     for (let i = 0; i < testString.length; i++) {
       if (testString[i] === testString[i + 1]) {
         if (currentRecord > prevRecord) {
+          longestString.push(testString[i]);
           prevRecord = currentRecord;
           prevString = longestString;
+          currentRecord = 0;
+          longestString = [];
         } else {
           currentRecord = 0;
           longestString = [];
         }
       } else {
-        longestString.push(testString[i + 1]);
+        longestString.push(testString[i]);
         currentRecord++;
       }
     }
